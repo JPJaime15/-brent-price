723775b2a9bc70e43aa255513395f18e3f2b8813
JPJaime15 <joao.mathidios@gmail.com>
2024-05-16 13:38:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ with open('modelo_brent.pkl', 'rb') as file_2:
 
 
 #Carregar o DataFrame
-df = pd.read_csv('data\ipea.csv')
+df = pd.read_csv('/mount/src/-brent-price/data/ipea.csv')
 df['Data'] = pd.to_datetime(df['Data'])
 df = df.sort_values(by='Data', ascending=True).reset_index(drop=True)
 
